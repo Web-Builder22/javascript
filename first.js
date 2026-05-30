36097a9567aa447c60d65ee2d07ae2a123e74a94
Noman <mnomi4884@gmail.com>
2026-05-30 15:25:03 +0500

    Add files via upload

---
+++ first.js
@@ -0,0 +1,17 @@
+let score=prompt("enter your score");
+if(score>=80&&score<=100){
+    console.log("A");
+
+}
+else if(score>=70&&score<=79){
+    console.log("B");
+}
+else if(score>=60&&score<=69){
+    console.log("C");
+}
+else if(score>=50&&score<=49){
+    console.log("D");
+}
+else{
+    console.log("fail");
+}
